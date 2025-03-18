07e3e7cba24f2f478556597347a0a8297e98125f
aisyyaaa <salsabilaam2@gmail.com>
2025-03-19 00:48:40 +0700

    Main.java

--- TUGAS/Main.java
+++ TUGAS/Main.java
@@ -1,23 +1,29 @@
+/*
+ * Nama File      : Main.java
+ * Pembuat        : Mutiara Aisyah Salsabila
+ * Tanggal        : 17 Maret 2025
+ */
+
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        DosenTetap dt = new DosenTetap("9545647548", "78647324", "Andi", 
-            LocalDate.of(1990, 5, 5), LocalDate.of(2015, 1, 1), 5000000, 
+        DosenTetap dt = new DosenTetap("4629104839", "78647324", "Diandra", 
+            LocalDate.of(1990, 5, 5), LocalDate.of(2013, 1, 1), 6000000, 
             "Fakultas Sains dan Matematika");
         dt.printInfo();
 
         System.out.println();
 
-        DosenTamu dtm = new DosenTamu("123456789", "DT-789", "Budi", 
-            LocalDate.of(1985, 8, 10), LocalDate.of(2020, 2, 15), 7000000, 
+        DosenTamu dtm = new DosenTamu("165930281", "DT-789", "Nurul", 
+            LocalDate.of(1985, 8, 10), LocalDate.of(2020, 6, 4), 8000000, 
             "Fakultas Teknik", LocalDate.of(2026, 12, 1));
         dtm.printInfo();
 
         System.out.println();
 
-        Tendik tendik = new Tendik("567890", "Siti", LocalDate.of(1985, 4, 15), 
-            LocalDate.of(2015, 7, 1), 6000000, "Administrasi");
+        Tendik tendik = new Tendik("265920", "Briana", LocalDate.of(1985, 6, 19), 
+            LocalDate.of(2015, 7, 1), 7000000, "Administrasi");
         tendik.printInfo();
     }
 }
